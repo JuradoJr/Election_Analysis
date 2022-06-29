7320778f2b97c60f4ed71cae3d9a6dc0bffa921d
JuradoJr <104862099+JuradoJr@users.noreply.github.com>
2022-06-29 16:48:04 -0700

    Delete Resources directory

--- Resources/# Create a filename variable to a direct.py
+++
@@ -1,7 +0,0 @@
-# Create a filename variable to a direct or indirect path to the file.
-import os
-
-
-file_to_save = os.path.join("analysis", "election_analysis.txt")
-# Using the open() function with the "w" mode we will write data to the file.
-open(file_to_save, "w")
